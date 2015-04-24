ef42019285ad6e6f518774fe6f3f7a6601de2f63
Khoa <senorkhoa@yahoo.com>
2015-04-23 18:11:53 -0600

    Added project files

---
+++ Weather/Weather.h
@@ -0,0 +1,31 @@
+#ifndef WEATHER_H_INCLUDED
+#define WEATHER_H_INCLUDED
+struct dayNode{
+    std::string date;
+    int high;
+    int low;
+    int percip;
+    int snow;
+    int snow_depth;
+    dayNode* next;
+};
+class Weather{
+public:
+    void addDay();
+    dayNode* getHead();
+    dayNode* getHigh();
+    dayNode* getTimeInterval(std::string start, std::string end); //returns head of a new separate linked list
+    dayNode* getLow();
+    dayNode* findDay(std::string time);
+    dayNode* getHighInterval(dayNode* head, std::string criteria);
+    dayNode* getHighSnow();
+    dayNode* getHighPercip();
+    dayNode* averageTemp(dayNode* head);
+    dayNode* averagePercip(dayNode* head);
+
+private:
+    dayNode* head;
+};
+
+
+#endif // WEATHER_H_INCLUDED
---
+++ Weather/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Weather.h"
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
